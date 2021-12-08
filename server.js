d433815b14706c4a43bea337113d4d0770b372cb
ChintalaNavaneeth <navaneethchintala1304@gmail.com>
2021-12-08 10:23:40 +0530

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -9,13 +10,19 @@ app.use(express.static('public'));
 const connectDB = require('./config/db.js');
 connectDB();
 
+//cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(','),
+};
+app.use(cors(corsOptions));
+
 //HTML Templates
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // Routes
 app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
+app.use('files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
 app.listen(PORT, () => {
